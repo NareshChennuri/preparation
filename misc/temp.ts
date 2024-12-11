41068ec45905bae1f77a035e13476a4d52429476
Naresh Chennuri <NareshChennuri@users.noreply.github.com>
2024-12-11 15:59:44 -0500

    xc

--- misc/temp.ts
+++ misc/temp.ts
@@ -1,11 +1,6 @@
-<div *ngIf="dataSource?.data?.length === 0" class="no-data-message">
-  No records found
-</div>
-
-
-.no-data-message {
-  text-align: center;
-  margin-top: 20px;
-  font-size: 16px;
-  color: gray;
+function parseInput(input) {
+  return input.split('\n').map(line => {
+    const [text, number] = line.split(' - ');
+    return { text, number: parseInt(number, 10) };
+  });
 }
