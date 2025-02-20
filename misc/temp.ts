436ee1b10b048bcdd400efd232c3224b8e029015
Naresh Chennuri <NareshChennuri@users.noreply.github.com>
2025-02-20 11:32:14 -0500

    sdf

--- misc/temp.ts
+++ misc/temp.ts
@@ -1,7 +1,33 @@
-formatTimeObject(time: { hour: number, minute: number, sec: number }): string {
-  const formattedHour = time.hour.toString().padStart(2, '0');
-  const formattedMinute = time.minute.toString().padStart(2, '0');
-  const formattedSecond = time.sec.toString().padStart(2, '0');
+<mat-menu #filterMenu="matMenu" class="custom-menu">
+  <div class="menu-header">
+    <span>Filter Options</span>
+    <button mat-icon-button (click)="filterMenu.close()">
+      <mat-icon>close</mat-icon>
+    </button>
+  </div>
 
-  return `${formattedHour}:${formattedMinute}:${formattedSecond}`;
-}
+  <mat-divider></mat-divider>
+
+  <mat-radio-group [(ngModel)]="selectedOption">
+    <mat-radio-button *ngFor="let option of radioOptions" [value]="option">
+      {{ option }}
+    </mat-radio-button>
+  </mat-radio-group>
+
+  <mat-divider></mat-divider>
+
+  <mat-checkbox *ngFor="let checkbox of checkBoxOptions" [(ngModel)]="checkbox.selected">
+    {{ checkbox.label }}
+  </mat-checkbox>
+
+  <mat-divider></mat-divider>
+
+  <div class="menu-actions">
+    <button mat-button (click)="applyFilters()">Apply</button>
+    <button mat-button (click)="clearFilters()">Cancel</button>
+  </div>
+</mat-menu>
+
+<button mat-button [matMenuTriggerFor]="filterMenu">
+  Open Filters
+</button>
