67adccc0f06eb80a7a9dd7f30276728a1d22663f
Naresh Chennuri <NareshChennuri@users.noreply.github.com>
2025-03-06 19:22:29 -0500

    asdf

--- misc/misc.ts
+++ misc/misc.ts
@@ -1,39 +1,105 @@
-function getUniqueStandardIds(data) {
-  const uniqueEntries = new Map();
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
-  data.forEach(entry => {
-      const key = `${entry.standardId}-${entry.createdDate}`;
-      if (!uniqueEntries.has(key)) {
-          uniqueEntries.set(key, { standardId: entry.standardId, createdDate: entry.createdDate });
-      }
-  });
-
-  return Array.from(uniqueEntries.values());
+interface ProgramOffering {
+  id: number;
+  lookupName: string;
+  longName: string;
+  shortName: string;
+  status: string;
+  refId: number;
 }
 
-// Example usage
-const data = [
-  { standardId: "NBKHHDX", createdDate: "2025-03-04", pageName: "TFGSignup" },
-  { standardId: "NBKHHDX", createdDate: "2025-03-04", pageName: "TFGCalendar" },
-  { standardId: "ZK1MAMS", createdDate: "2025-03-03", pageName: "TFGCalendar" },
-  { standardId: "ZK2CA7B", createdDate: "2025-03-04", pageName: "TFGSignup" },
-  { standardId: "ZK2CA7B", createdDate: "2025-03-04", pageName: "TFGCalendar" },
-  { standardId: "ZK2J6KI", createdDate: "2025-03-03", pageName: "TFGSignup" },
-  { standardId: "ZK2J6KI", createdDate: "2025-03-03", pageName: "TFGCalendar" },
-  { standardId: "ZK7GGQZ", createdDate: "2025-03-03", pageName: "TFGSignup" },
-  { standardId: "ZK7GGQZ", createdDate: "2025-03-03", pageName: "TFGCalendar" },
-  { standardId: "ZKJA2IN", createdDate: "2025-03-03", pageName: "TFGSignup" },
-  { standardId: "ZKJA2IN", createdDate: "2025-03-03", pageName: "TFGCalendar" },
-  { standardId: "ZKNGSHW", createdDate: "2025-03-04", pageName: "TFGSignup" },
-  { standardId: "ZKNGSHW", createdDate: "2025-03-04", pageName: "TFGCalendar" },
-  { standardId: "ZKS08VZ", createdDate: "2025-03-03", pageName: "TFGSignup" },
-  { standardId: "ZKS08VZ", createdDate: "2025-03-03", pageName: "TFGCalendar" },
-  { standardId: "ZKW5PJD", createdDate: "2025-03-04", pageName: "TFGSignup" },
-  { standardId: "ZKW5PJD", createdDate: "2025-03-04", pageName: "TFGCalendar" },
-  { standardId: "ZKXE8R9", createdDate: "2025-03-03", pageName: "TFGSignup" },
-  { standardId: "ZKYCUBS", createdDate: "2025-03-03", pageName: "TFGSignup" },
-  { standardId: "ZKYCUBS", createdDate: "2025-03-03", pageName: "TFGCalendar" },
-  { standardId: "ZKYLBKT", createdDate: "2025-03-03", pageName: "TFGSignup" }
-];
+interface DropdownOption {
+  type: 'option' | 'group';
+  label?: string;
+  value?: string;
+  options?: { value: string; viewValue: string }[];
+}
 
-console.log(getUniqueStandardIds(data));
+@Component({
+  selector: 'app-event-dropdown',
+  templateUrl: './event-dropdown.component.html',
+  styleUrls: ['./event-dropdown.component.scss']
+})
+export class EventDropdownComponent {
+  eventControl = new FormControl();
+  dropdownOptions: DropdownOption[] = [];
+
+  programOfferings: ProgramOffering[] = [
+    {"id": 40, "lookupName": "ProgramOfferings", "longName": "Cohorts", "shortName": "COHORTS", "status": "A", "refId": 0},
+    {"id": 41, "lookupName": "ProgramOfferings", "longName": "Office Hours", "shortName": "OFFICEHRS", "status": "A", "refId": 0},
+    {"id": 113, "lookupName": "ProgramOfferings", "longName": "Tech Talks", "shortName": "TECHTALKS", "status": "A", "refId": 0},
+    {"id": 133, "lookupName": "ProgramOfferings", "longName": "Vendor Workshops", "shortName": "VENDORWORKSHOPS", "status": "A", "refId": 0},
+    {"id": 134, "lookupName": "ProgramOfferings", "longName": "Facilitated Training", "shortName": "FACILITATEDTRNG", "status": "A", "refId": 0},
+    {"id": 135, "lookupName": "ProgramOfferings", "longName": "Competitions", "shortName": "COMPETATIONS", "status": "A", "refId": 135},
+    {"id": 179, "lookupName": "ProgramOfferings", "longName": "Quizzes", "shortName": "QUIZZES", "status": "A", "refId": 135},
+    {"id": 180, "lookupName": "ProgramOfferings", "longName": "Codeathon", "shortName": "CODEATHON", "status": "A", "refId": 135},
+    {"id": 181, "lookupName": "ProgramOfferings", "longName": "Crosswords", "shortName": "CROSSWORDS", "status": "A", "refId": 135},
+    {"id": 182, "lookupName": "ProgramOfferings", "longName": "Technical Contest", "shortName": "TECHCONTEST", "status": "A", "refId": 135}
+  ];
+
+  constructor() {
+    this.dropdownOptions = this.buildDropdownOptions(this.programOfferings);
+  }
+
+  buildDropdownOptions(data: ProgramOffering[]): DropdownOption[] {
+    let groupedOptions: DropdownOption[] = [];
+    let groups: { [key: number]: DropdownOption } = {};
+
+    // Create groups
+    data.forEach((item) => {
+      if (item.id === item.refId) {
+        groups[item.id] = {
+          type: "group",
+          label: item.longName,
+          options: []
+        };
+      }
+    });
+
+    // Assign sub-options to their respective groups
+    data.forEach((item) => {
+      if (item.id !== item.refId && groups[item.refId]) {
+        groups[item.refId].options?.push({
+          value: item.shortName,
+          viewValue: item.longName
+        });
+      } else if (item.refId === 0) {
+        // Standalone options
+        groupedOptions.push({
+          type: "option",
+          value: item.shortName,
+          label: item.longName
+        });
+      }
+    });
+
+    // Convert groups to an array
+    Object.values(groups).forEach((group) => {
+      groupedOptions.push(group);
+    });
+
+    return groupedOptions;
+  }
+}
+
+
+<mat-form-field>
+  <mat-label>Select an Event</mat-label>
+  <mat-select [formControl]="eventControl">
+    <!-- Normal options (No Group) -->
+    <ng-container *ngFor="let option of dropdownOptions">
+      <mat-option *ngIf="option.type === 'option'" [value]="option.value">
+        {{ option.label }}
+      </mat-option>
+
+      <!-- Grouped options -->
+      <mat-optgroup *ngIf="option.type === 'group'" [label]="option.label">
+        <mat-option *ngFor="let subOption of option.options" [value]="subOption.value">
+          {{ subOption.viewValue }}
+        </mat-option>
+      </mat-optgroup>
+    </ng-container>
+  </mat-select>
+</mat-form-field>
