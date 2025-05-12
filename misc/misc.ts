8ec7d707e392bdb3034fb8c8d8d86f6b72ccef0b
Naresh Chennuri <NareshChennuri@users.noreply.github.com>
2025-05-12 17:05:45 -0400

    asdf

--- misc/misc.ts
+++ misc/misc.ts
@@ -62,3 +62,10 @@ export function getNextOccurrenceAndExDatesFromISO(
     return { nextOccurrenceFormatted: null, updatedExDates: exDates };
   }
 }
+
+
+function parseIcalDate(str: string): Date {
+  // Input: '20250601T120000Z'
+  const formatted = `${str.slice(0, 4)}-${str.slice(4, 6)}-${str.slice(6, 8)}T${str.slice(9, 11)}:${str.slice(11, 13)}:${str.slice(13, 15)}Z`;
+  return new Date(formatted); // Now it’s valid
+}
